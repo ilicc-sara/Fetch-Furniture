7afffadcc4d8931ce11ba222eee87f9f681c0d92
Sara <sarailic431@gmail.com>
2025-04-14 18:59:32 +0200

    finished in two ways

--- src/main.js
+++ src/main.js
@@ -2,14 +2,6 @@
 import "./style.css";
 
 const furnitureList = document.querySelector(".furniture-list");
-const pictire = document.querySelector(".furniture-picture");
-const name = document.querySelector(".furniture-name");
-const price = document.querySelector(".furniture-price");
-
-// function capitalizeFirst(str) {
-//   if (!str) return "";
-//   return str.charAt(0).toUpperCase() + str.slice(1);
-// }
 
 function capitalizeEveryWord(str) {
   return str
@@ -22,9 +14,14 @@ fetch(
   // "https://www.course-api.com/react-store-single-product?id=recd1jIVIEChmiwhe"
   "https://www.course-api.com/react-store-products"
 )
-  .then((response) => response.json())
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    return response.json();
+  })
   .then((data) => {
-    console.log(data);
     data.forEach((furniture) => {
       let item = document.createElement("li");
       item.innerHTML = `<img
@@ -44,3 +41,39 @@ fetch(
     });
   })
   .catch((error) => console.error(error));
+
+////////////////////////////////////////////////////
+window.fetchData = async function () {
+  try {
+    const response = await fetch(
+      "https://www.course-api.com/react-store-products"
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    const data = await response.json();
+    data.forEach((furniture) => {
+      let item = document.createElement("li");
+      item.innerHTML = `<img
+            src="${furniture.image}"
+            alt="Furniture-Picture"
+            class="furniture-picture"
+            style="display: block"
+          />
+          <div class="furniture-item-info">
+            <h3 class="furniture-name">${capitalizeEveryWord(
+              furniture.name
+            )}</h3>
+            <h3 class="furniture-price">${+furniture.price / 100} $</h3>
+          </div>`;
+      item.className = "furniture-item";
+      furnitureList.appendChild(item);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchData();
