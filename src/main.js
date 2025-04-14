029196e1fd09ebd5a41573afe0249ae1a603e3e0
Sara <sarailic431@gmail.com>
2025-04-14 16:07:26 +0200

    fetching furniture data with url

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 "use strict";
 import "./style.css";
+
+const furnitureList = document.querySelector(".furniture-list");
+const pictire = document.querySelector(".furniture-picture");
+const name = document.querySelector(".furniture-name");
+const price = document.querySelector(".furniture-price");
+
+fetch(
+  // "https://www.course-api.com/react-store-single-product?id=recd1jIVIEChmiwhe"
+  "https://www.course-api.com/react-store-products"
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data[0].image))
+  .catch((error) => console.error(error));
